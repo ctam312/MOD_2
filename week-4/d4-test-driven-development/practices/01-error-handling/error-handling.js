17a9cfe4a26495dc6316e4f7b5f9b84df51d0a9d
Ctam312 <ctam312@gmail.com>
2022-10-20 16:20:49 -0400

    W4D4 code update

--- week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
+++ week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
@@ -1,19 +1,41 @@
 // 1.
 function sum(array) {
   let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+  try{
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  } catch (error){
+    if (error instanceof TypeError){
+      console.log(error.name + ': ' + error.message)
+    }
   }
-  return sum;
 }
+
 
 let res = sum(null);
 console.log(res);
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+
+function sayName(name){
+  if (typeof name === 'string'){
+    console.log(name);
+  } else {
+    throw new TypeError("Invalid name! Must be a string!")
+  }
+}
+
+
+try {
+  sayName("Alex");
+  sayName(1);
+} catch (e) {
+  console.error(e.message);
+}
+
 // Your code here
 
 // 3.
@@ -23,4 +45,12 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try{
+  greet('Hey there!');
+  greet(1);
+  greet();
+} catch {
+  console.log('Hello World!')
 }
--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,14 +1,33 @@
 function isFive(num) {
-  // Your code here
+	// Your code here
+	if (num === 5) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function isOdd(number) {
-  // Your code here
+	if (typeof number !== "number") {
+		throw new TypeError("Invalid, must use a number");
+	} else {
+		if (number % 2 !== 0) {
+			return true;
+		} else if (number % 2 === 0) {
+			return false;
+		}
+	}
 }
 
-function myRange(min, max, step = 1) {
-  // Your code here
+function myRange(min, max, step = 1, arr = []) {
+	// Your code here
+  if (min > max){
+    return arr;
+  } else {
+    arr.push(min);
+    min += step;
+    return myRange(min, max, step, arr);
+  }
 }
-
 
 module.exports = { isFive, isOdd, myRange };
