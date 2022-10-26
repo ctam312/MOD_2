6a6f04c5bf445928f6affe97f3f23bcfbf9db85b
Ctam312 <ctam312@gmail.com>
2022-10-26 16:00:50 -0400

    linked list stuff

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
@@ -1,35 +1,66 @@
 class DoublyLinkedListNode {
-  constructor(val) {
-    this.value = val;
-    this.prev = null;
-    this.next = null;
-  }
+	constructor(val) {
+		this.value = val;
+		this.prev = null;
+		this.next = null;
+	}
 }
 
 class DoublyLinkedList {
-  constructor() {
-    // Your code here
-  }
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
 
-  addToHead(val) {
-    // Your code here
-  }
+	addToHead(val) {
+		// Your code here
+    const start = Date.now();
+		const newNode = new DoublyLinkedListNode(val);
+    this.length++
 
-  addToTail(val) {
-    // Your code here
-  }
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head.prev = newNode;
+			this.head = newNode;
+		}
+    const end = Date.now();
+    console.log(end-start)
+	}
 
-  // You can use this function to help debug
-  print() {
-    let current = this.head;
+	addToTail(val) {
+		// Your code here
+    const start2 = Date.now();
+		const newNode = new DoublyLinkedListNode(val);
+		this.length++;
 
-    while (current) {
-      process.stdout.write(`${current.value} <-> `);
-      current = current.next;
-    }
+		//case 1: there are not any nodes in the list
+		if (!this.tail) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.prev = this.tail;
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+    const end2 =Date.now();
+    console.log(end2-start2);
+	}
 
-    console.log("NULL");
-  }
+	// You can use this function to help debug
+	print() {
+		let current = this.head;
+
+		while (current) {
+			process.stdout.write(`${current.value} <-> `);
+			current = current.next;
+		}
+
+		console.log("NULL");
+	}
 }
 
 module.exports = DoublyLinkedList;
--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -1,34 +1,59 @@
 class LinkedListNode {
-  constructor(val) {
-    this.value = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.value = val;
+		this.next = null;
+	}
 }
 
 class LinkedList {
-  constructor() {
-    // Your code here
-  }
+	constructor() {
+		this.head = null;
+    this.length = 0;
+	}
+  
+	addToHead(val) {
+    const start = Date.now();
+    const newNode = new LinkedListNode(val);
 
-  addToHead(val) {
-    // Your code here
-  }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    this.print();
+    
+	}
 
-  addToTail(val) {
-    // Your code here
-  }
+	addToTail(val) {
 
-  // You can use this function to help debug
-  print() {
-    let current = this.head;
-
-    while (current) {
-      process.stdout.write(`${current.value} -> `);
-      current = current.next;
+		const newNode = new LinkedListNode(val);
+    this.length++;
+    //case 1: there are not any nodes in the list
+    if (!this.head){
+      this.head = newNode;
+      return this;
     }
 
-    console.log("NULL");
-  }
+    let currentNode = this.head
+
+    while (currentNode.next){
+      currentNode = currentNode.next
+    }
+
+    currentNode.next = newNode;
+    this.print();
+    //case 2: there are nodes in the list
+	}
+
+	// You can use this function to help debug
+	print() {
+		let current = this.head;
+
+		while (current) {
+			process.stdout.write(`${current.value} -> `);
+			current = current.next;
+		}
+
+		console.log("NULL");
+	}
 }
 
 module.exports = LinkedList;
