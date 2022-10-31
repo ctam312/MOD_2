dc51d5410f4b72f881ad78329407770308a77e6a
Ctam312 <ctam312@gmail.com>
2022-10-31 15:44:10 -0400

    idk update

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -1,40 +1,76 @@
-const sha256 = require('js-sha256');
+const sha256 = require("js-sha256");
 
 class KeyValuePair {
-  constructor(key, value) {
-    this.key = key;
-    this.value = value;
-    this.next = null;
-  }
+	constructor(key, value) {
+		this.key = key;
+		this.value = value;
+		this.next = null;
+	}
 }
 
 class HashTable {
+	constructor(numBuckets = 4) {
+		this.count = 0;
+		this.capacity = numBuckets;
+		this.data = new Array(this.capacity).fill(null);
+	}
 
-  constructor(numBuckets = 4) {
-    // Your code here
-  }
+	hash(key) {
+		// Your code here
+		const hash = sha256(key);
+		const first8 = hash.slice(0, 8);
+		const decimal = parseInt(first8, 16);
+		return decimal;
 
-  hash(key) {
-    // Your code here
-  }
+		//return parseInt(sha256(key).slice(0,8), 16);
+	}
 
-  hashMod(key) {
-    // Your code here
-  }
+	hashMod(key) {
+		// Your code here
+		return this.hash(key) % this.capacity;
+	}
 
-  insertNoCollisions(key, value) {
-    // Your code here
-  }
+	insertNoCollisions(key, value) {
+		const newPair = new KeyValuePair(key, value);
+		const idx = this.hashMod(key);
 
-  insertWithHashCollisions(key, value) {
-    // Your code here
-  }
+		if (this.data[idx])
+			throw new Error("hash collision or same key/value pair already exists!");
+		this.data[idx] = newPair;
+		this.count++;
+	}
 
-  insert(key, value) {
-    // Your code here
-  }
+	insertWithHashCollisions(key, value) {
+		// Your code here
+		const newPair = new KeyValuePair(key, value);
+		const idx = this.hashMod(key);
+		if (!this.data[idx]) {
+			this.data[idx] = newPair;
+		} else {
+			newPair.next = this.data[idx];
+			this.data[idx] = newPair;
+		}
+		this.count++;
+	}
 
+	insert(key, value) {
+		// Your code here
+		const idx = this.hashMod(key);
+		let currentPair = this.data[idx]; // -> kvp
+		while (currentPair && currentPair.key !== key) {
+			currentPair = currentPair.next;
+		}
+
+		if (currentPair) {
+			currentPair.value = value;
+			return this;
+		}
+
+    const newPair = new KeyValuePair(key, value);
+    if (this.data[idx]) newPair.next = this.data[idx];
+    this.data[idx] = newPair;
+    this.count++
+	}
 }
-
 
 module.exports = HashTable;
